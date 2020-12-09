4c9fb84089a246b65f27c194c189349dcc4848ac
Melvil Rigot <74654893+RigotMelvil@users.noreply.github.com>
2020-12-09 12:41:07 +0100

    Motus V:0.7
    vertion jouable
    -la reponce est 4 foie "compte"

--- Motus_Py.py
+++ Motus_Py.py
@@ -34,23 +34,32 @@ lettre_aleatoire(tableau,lettres)
 # ----- Code Principal
 
 while (conditon_victoir == 0):
-    
-
+   
     resultat_1 = 0
     resultat_2 = 0
     resultat_3 = 0
     resultat_4 = 0
  
-    choix_position = int(input(" ou aller :"))
-    nouvelle_lettre = str(input(" remplacer par :"))
+ # ----- Affichage
+    for ligne in range(0,4):
+                for colone in range(0,6):
+                    print(Fore.BLUE + Style.BRIGHT + " ",tableau[colone+6*ligne], end='')
+                print(Style.RESET_ALL)
+
+    print()
+#print(resultat_1,resultat_2,resultat_3,resultat_4)
+ 
+    choix_position = int(input(" Quelle lettre voulez vous changer?(1 à 24)  :"))-1
+    nouvelle_lettre = str(input(" Par quelle lettre?  :"))
     tableau[choix_position] = str(nouvelle_lettre)
+    print()
  
     #verif
     for i in range(0,24):
         if(i > 0 and i <= 6):
             for j in range(0,6):
                 if (tableau[i-1] == mot_mystere_1[j]):
-                    resultat_1 = resultat_1 + 1
+                    resultat_1 = resultat_1 + 1      
         if(i > 6 and i <= 12):
                 for j in range(0,6):
                     if (tableau[i-1] == mot_mystere_2[j]):
@@ -63,14 +72,13 @@ while (conditon_victoir == 0):
                 for j in range(0,6):
                     if (tableau[i] == mot_mystere_4[j]):
                         resultat_4 = resultat_4 + 1
-    # ----- Affichage
-    for ligne in range(0,4):
-                for colone in range(0,6):
-                    print(Fore.BLUE + Style.BRIGHT + " ",tableau[colone+6*ligne], end='')
-                print(Style.RESET_ALL)
-    print(resultat_1,resultat_2,resultat_3,resultat_4)
+                        
+    # ----- Condition de Victoir  
+    resultat_totatle = int(resultat_1+resultat_2+resultat_3+resultat_4)
+    if (resultat_totatle == 24):
+        conditon_victoir = 1
 
-
+print(" Vous avez trouver tout les mots!")
 
 # ----- Code Fin
 input()
